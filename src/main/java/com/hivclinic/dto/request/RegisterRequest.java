f272a054d93575b720f905cbd6da5f64909bf85d
Pham phuoc an <anphamgm2k5@gmail.com>
2025-06-18 09:55:36 +0700

    reinstall  node-modules

--- src/main/java/com/hivclinic/dto/request/RegisterRequest.java
+++ src/main/java/com/hivclinic/dto/request/RegisterRequest.java
@@ -19,11 +19,13 @@ public class RegisterRequest {
     @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
     @Size(max = 100, message = "Email must not exceed 100 characters")
-    private String email;
-
-    @NotBlank(message = "Password is required")
+    private String email;    @NotBlank(message = "Password is required")
     @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
     private String password;
+
+    @NotBlank(message = "Password confirmation is required")
+    @Size(min = 6, max = 100, message = "Password confirmation must match password")
+    private String confirmPassword;
 
     @NotBlank(message = "First name is required")
     @Size(max = 50, message = "First name must not exceed 50 characters")
